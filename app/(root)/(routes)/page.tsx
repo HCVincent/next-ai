bd1f4981861cd02db11c650edfd8ed8e24d3226a
HCVincent <lilacsweet@163.com>
2023-10-06 20:46:40 +0800

    category filter

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,13 +1,14 @@
+import Categories from "@/components/categories";
 import { SearchInput } from "@/components/search-input";
-import { UserButton } from "@clerk/nextjs";
+import prismadb from "@/lib/prismadb";
 import React from "react";
 
-type rootPageProps = {};
-
-const RootPage: React.FC<rootPageProps> = () => {
+const RootPage = async () => {
+  const categories = await prismadb.category.findMany();
   return (
     <div className="h-full p-4 space-y-2">
       <SearchInput />
+      <Categories data={categories} />
     </div>
   );
 };
